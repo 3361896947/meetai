a4949c7a212b44c9518714d8e6ede3064b3abe04
Liao <3361896947a@gmail.com>
2025-10-18 17:41:55 +0800

    08：tRPC配置

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,29 +1,11 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export function HomeView() {
-  const { data: session } = authClient.useSession();
-  const router = useRouter();
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "Liao" }));
 
-  if (!session) return <p>正在加载</p>;
-
-  return (
-    <div className="p-4 flex flex-col gap-4">
-      <p>欢迎, {session.user.name}</p>
-      <Button
-        onClick={() =>
-          authClient.signOut({
-            fetchOptions: {
-              onSuccess: () => router.push("/sign-in"),
-            },
-          })
-        }
-      >
-        登出
-      </Button>
-    </div>
-  );
+  return <div className="p-4 flex flex-col gap-4">{data?.greeting}</div>;
 }
